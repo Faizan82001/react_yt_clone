ff5b8f10c6d054e0e41b7b7ee6acb7b44f4e4dfe
Faizan <faizan.gigani@marsintel.com>
2024-02-25 12:22:01 +0530

    first commit

---
+++ src/utils/api.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key": "9fae113149msh7cc5f04faf5ab64p156517jsnb34abd3fd049",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
